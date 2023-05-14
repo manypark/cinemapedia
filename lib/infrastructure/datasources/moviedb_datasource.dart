d5927d4cbccb9de93fc0b678d73cbcfe8f701e29
Manu <manypark@live.com>
2023-05-13 22:49:03 -0600

    feat: infra and mapper implements

---
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -0,0 +1,31 @@
+import 'package:cinemapedia/config/constans/enviroment.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
+import 'package:dio/dio.dart';
+
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+
+class MoviedbDataSource extends MovieDataSource {
+
+  final dio = Dio( BaseOptions(
+    baseUrl: 'https://api.themoviedb.org/3',
+    queryParameters: {
+      'api_key' : Environment.movieDBKey,
+      'language': 'es-MX'
+    }
+  ));
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    
+    final response = await dio.get('/movie/now_playing');
+
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies = movieDBResponse.results.map((moviedb) => MovieMapper.movieDBToEntity(moviedb)).toList();
+    
+    return movies;
+  }
+
+}
---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,29 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+
+  static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
+      id              : moviedb.id,
+      adult           : moviedb.adult,
+      backdropPath    : getImagePath(moviedb.backdropPath),
+      genreIds        : moviedb.genreIds.map((e) => e.toString()).toList(),
+      originalLanguage: moviedb.originalLanguage,
+      originalTitle   : moviedb.originalTitle,
+      overview        : moviedb.overview,
+      popularity      : moviedb.popularity,
+      posterPath      : getImagePath(moviedb.posterPath),
+      releaseDate     : moviedb.releaseDate,
+      title           : moviedb.title,
+      video           : moviedb.video,
+      voteAverage     : moviedb.voteAverage,
+      voteCount       : moviedb.voteCount
+  );
+
+  static String getImagePath( String imagePath ) {
+    return (imagePath != '')
+      ? 'https://image.tmdb.org/t/p/w500$imagePath'
+      : 'https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg';
+  }
+
+}
