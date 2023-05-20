99050e5445678f05bf5169d74ba2373efe371048
Manu <manypark@live.com>
2023-05-19 23:19:13 -0600

    feat: new route, ontap image

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -5,7 +5,20 @@ final appRouter = GoRouter(
   initialLocation: '/',
   routes         : [
 
-    GoRoute(path: '/', name: HomeScreen.name, builder: (context, state) => const HomeScreen() , ),
+    GoRoute(
+      path   : '/', 
+      name   : HomeScreen.name, 
+      builder: (context, state) => const HomeScreen(),
+    ),
+
+    GoRoute(
+      path   : '/movie/:id', 
+      name   : MovieScreen.name, 
+      builder: (context, state) {
+        final movieId = state.pathParameters['id'] ?? 'no-id';
+        return MovieScreen( movieId: movieId );
+      }
+    ),
     
   ]
 );
