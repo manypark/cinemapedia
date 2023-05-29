6a6d615f2a54e11b513a10d5c289c2e58de42930
Manu <manypark@live.com>
2023-05-29 17:26:42 -0600

    feat: go router official implementation

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,124 +1,22 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
-import 'package:cinemapedia/presentation/providers/providers.dart';
-
 
 class HomeScreen extends StatelessWidget {
 
   static const name = 'home-screen';
 
-  const HomeScreen({super.key});
+  final Widget childView;
+
+  const HomeScreen({
+    super.key,
+    required this.childView
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body               : _HomeView(),
-      bottomNavigationBar: CustomBottomNavigation(),
+    return Scaffold(
+      body               : childView,
+      bottomNavigationBar: const CustomBottomNavigation(),
     );
-  }
-}
-
-class _HomeView extends ConsumerStatefulWidget {
-
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-
-  @override
-  void initState() {
-    super.initState();
-    ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
-    ref.read( popularMoviesProvider.notifier    ).loadNextPage();
-    ref.read( upcomingMoviesProvider.notifier   ).loadNextPage();
-    ref.read( topRateMoviesProvider.notifier    ).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    final initialLoading    = ref.watch( initialLoadingProvider  );
-    if(initialLoading) return const FullScreenLoader();
-
-    final moviesSlideShow   = ref.watch( moviesSlideShowProvider  );
-
-    final nowPlayingMovies  = ref.watch( nowPlayingMoviesProvider );
-    final popularMovies     = ref.watch( popularMoviesProvider    );
-    final upcomingMovies    = ref.watch( upcomingMoviesProvider   );
-    final topRatedMovies    = ref.watch( topRateMoviesProvider    );
-
-    return CustomScrollView(
-      slivers: [
-
-        const SliverAppBar(
-          floating     : true,
-          flexibleSpace: FlexibleSpaceBar(
-            title: CustomAppBar(),
-          ),
-        ),
-
-        SliverList(
-          delegate: SliverChildBuilderDelegate(
-            (context, index) {
-
-              return Column(
-                children: [
-            
-                  MoviesslideShow( movies: moviesSlideShow ),
-            
-                  MovieHorizontalListView(
-                    movies      : nowPlayingMovies,
-                    title       : 'En cines',
-                    subTitle    : 'Lunes 20',
-                    loadNextPage: () {
-                      ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
-                    },
-                  ),
-            
-                  MovieHorizontalListView(
-                    movies      : upcomingMovies,
-                    title       : 'Próximamente',
-                    subTitle    : 'En este mes',
-                    loadNextPage: () {
-                      ref.read( upcomingMoviesProvider.notifier ).loadNextPage();
-                    },
-                  ),
-            
-                  MovieHorizontalListView(
-                    movies      : popularMovies,
-                    title       : 'Populares',
-                    // subTitle    : 'En este mes',
-                    loadNextPage: () {
-                      ref.read( popularMoviesProvider.notifier ).loadNextPage();
-                    },
-                  ),
-            
-                  MovieHorizontalListView(
-                    movies      : topRatedMovies,
-                    title       : 'Mejor calificadas',
-                    subTitle    : 'De todos los tiempos',
-                    loadNextPage: () {
-                      ref.read( topRateMoviesProvider.notifier ).loadNextPage();
-                    },
-                  ),
-
-                  const SizedBox( height: 20.0 ,),
-                ],
-              );
-    
-            },
-            childCount: 1,
-          ),
-        ),
-
-      ]
-    );
-
   }
 }
---
+++ lib/presentation/views/home_views/views.dart
@@ -0,0 +1,2 @@
+export 'package:cinemapedia/presentation/views/home_views/home_views.dart';
+export 'package:cinemapedia/presentation/views/home_views/favorites_view.dart';
